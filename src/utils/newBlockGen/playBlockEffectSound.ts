e08d94884900fb33c2758d8fa28ef6692183a43d
tomimarkus991 <tomimarkusalber@gmail.com>
2021-03-31 17:18:42 +0300

    made soundeffects better

--- src/utils/newBlockGen/playBlockEffectSound.ts
+++ src/utils/newBlockGen/playBlockEffectSound.ts
@@ -1,27 +1,26 @@
-// import blockEffect1 from "../../assets/audio/blockEffect1.mp3";
-// import blockEffect2 from "../../assets/audio/blockEffect2.mp3";
-// import blockEffect3 from "../../assets/audio/blockEffect3.mp3";
+import { Distortion, Player } from "tone";
+import blockEffect1 from "../../assets/audio/blockEffect1.mp3";
+import blockEffect2 from "../../assets/audio/blockEffect2.mp3";
+import blockEffect3 from "../../assets/audio/blockEffect3.mp3";
+
 export let playBlockEffectSound = () => {
-  // let randomSound = Math.floor(Math.random() * Math.floor(3)) + 1;
-  // let player;
-  // if (randomSound === 1) {
-  //   player = new Tone.Player({
-  //     url: "../../assets/audio/blockEffect1.mp3",
-  //     autostart: true,
-  //   });
-  // } else if (randomSound === 2) {
-  //   player = new Tone.Player({
-  //     url: "../../assets/audio/blockEffect2.mp3",
-  //     autostart: true,
-  //   });
-  // } else if (randomSound === 3) {
-  //   player = new Tone.Player({
-  //     url: "../../assets/audio/blockEffect3.mp3",
-  //     autostart: true,
-  //   });
-  // }
-  // let distortion = new Tone.Distortion(0).toDestination();
-  // if (player) {
-  //   player.connect(distortion);
-  // }
+  let randomSound = Math.floor(Math.random() * Math.floor(3)) + 1;
+  let player;
+
+  let url = blockEffect1;
+  if (randomSound === 1) {
+    url = blockEffect1;
+  } else if (randomSound === 2) {
+    url = blockEffect2;
+  } else if (randomSound === 3) {
+    url = blockEffect3;
+  }
+  player = new Player({
+    url: url,
+    autostart: true,
+  });
+  let distortion = new Distortion(0).toDestination();
+  if (player) {
+    player.connect(distortion);
+  }
 };
